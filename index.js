bf20d0c4ba26e3304255308c14d4ea4179fa4dfc
Margarita Ramirez <margarita26.mr@gmail.com>
2024-02-22 13:44:23 -0500

    Save File

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution in this file!
+
+function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = cloneObject(employee);
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  const employee = {
+    name: 'John Doe',
+    age: 30,
+    department: 'Engineering'
+  };
+  
+  describe('employees', () => {
+    let employee;
+  
+    beforeEach(() => {
+      
+      employee = {
+        name: 'John Doe',
+        age: 30,
+        department: 'Engineering'
+      };
+    });
+  
+    it('updateEmployeeWithKeyAndValue(employee, key, value)', () => {
+     
+    });
+  
+    it('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', () => {
+    
+    });
+  
+    it('deleteFromEmployeeByKey(employee, key)', () => {
+    
+    });
+  
+    it('destructivelyDeleteFromEmployeeByKey(employee, key)', () => {
+    });
+  });
+  
